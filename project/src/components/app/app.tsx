267ec3b038d82897fb66ac9268e1716b405b7958
vetrov78 <vettrov78@gmail.com>
2022-10-26 11:58:16 +0300

    Добавляет маршруты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,16 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LayoutScreen from '../../pages/layout-sreen/layout-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
-// import LoginScreen from '../../pages/login-screen/login-screen';
-// import PropertyScreen from '../../pages/property-screen/property-screen';
 
 type AppScreenProps = {
   offersNumber: number;
 }
 
 function App({offersNumber}: AppScreenProps): JSX.Element {
+  const isLoginPage = true;
+
   return (
-    <MainScreen offersNumber={offersNumber}/>
-    // <LoginScreen />
-    // <PropertyScreen />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LayoutScreen isLoginPage={isLoginPage}/>}>
+          <Route index element={<MainScreen offersNumber={offersNumber}/>} />
+          <Route path='/login' element={<LoginScreen />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
