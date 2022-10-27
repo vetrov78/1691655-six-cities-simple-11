37d0fca4bd8c232fe31da61b76a8352580b7b258
Andrei Vetrov <vettrov78@gmail.com>
2022-10-27 10:09:22 +0300

    Добавляет маршруты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LayoutScreen from '../../pages/layout-sreen/layout-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PropertyScreen from '../../pages/property-screen/property-screen';
 
 type AppScreenProps = {
   offersNumber: number;
 }
 
 function App({offersNumber}: AppScreenProps): JSX.Element {
-  const isLoginPage = true;
+  const isLoginPage = false;
 
   return (
     <BrowserRouter>
@@ -16,6 +18,8 @@ function App({offersNumber}: AppScreenProps): JSX.Element {
         <Route path='/' element={<LayoutScreen isLoginPage={isLoginPage}/>}>
           <Route index element={<MainScreen offersNumber={offersNumber}/>} />
           <Route path='/login' element={<LoginScreen />} />
+          <Route path='/offer:id' element={<PropertyScreen />} />
+          <Route path='*' element={<NotFoundScreen />} />
         </Route>
       </Routes>
     </BrowserRouter>
