e9da6b20f7de71fa12ef841080ddd3521ebc048a
Andrei Vetrov <vettrov78@gmail.com>
2022-11-11 11:35:59 +0300

    Добавляет сортировку

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,6 +1,6 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { offers } from '../mocks/offers';
-import { Offer } from '../types/offer-type';
+import { Offer, Offers } from '../types/offer-type';
 import { changeCity, changeSortType, openCloseSorting } from './actions';
 
 const initialState = {
@@ -14,21 +14,36 @@ const sortPriceStartHigh = (offer1: Offer, offer2: Offer) => {
   const {price: priceOne} = offer1;
   const {price: priceTwo} = offer2;
 
+  return (priceOne < priceTwo) ? 1 : -1;
+};
+
+const sortPriceStartLow = (offer1: Offer, offer2: Offer) => {
+  const {price: priceOne} = offer1;
+  const {price: priceTwo} = offer2;
+
   return (priceOne > priceTwo) ? 1 : -1;
 };
 
-const sortedOffers = (currentOffers: Offer[], currentCity: string, currentSortedType: string, sortingType: string) => {
+const sortByRating = (offer1: Offer, offer2: Offer) => {
+  const {rating: ratingOne} = offer1;
+  const {rating: ratingTwo} = offer2;
+
+  return (ratingOne < ratingTwo) ? 1 : -1;
+}
+
+const sortedOffers = (currentOffers: Offer[], currentCity: string, currentSortedType: string, sortingType: string): Offers => {
   if (sortingType === currentSortedType) {
     return offers;
   } else {
     switch(sortingType) {
       case 'Popular':
-        // eslint-disable-next-line no-console
-        console.log(currentCity);
-
         return offers.filter((offer) => offer.city.name === currentCity);
       case 'Price: low to high':
+        return currentOffers.sort(sortPriceStartLow);
+      case 'Price: high to low':
         return currentOffers.sort(sortPriceStartHigh);
+      case 'Top rated first':
+        return currentOffers.sort(sortByRating);
     }
   }
   return currentOffers;
