9b99dcac6667ffeece39745ba14cd3f349190526
Andrei Vetrov <vettrov78@gmail.com>
2022-12-01 11:13:53 +0300

    Добавляет экран при отсутствии предложений

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -14,12 +14,14 @@ import { getCity, getProcessedOffers } from '../../store/app-process/selectors';
 function MainScreen (): JSX.Element {
   //Добавляет стили для отсутствия прокрутки у блока с карточками, и вся карта видна на экране
   const root = document.getElementById('root') as HTMLElement;
-  root.style.cssText = 'display: flex; flex-direction: column; overflow-y: hidden';
+  root.style.cssText = 'display: flex; flex-direction: column; overflow-y: hidden; height: inherit;';
 
   // необходим для изменения цвета маркера на карте при наведении на соответствующее предложение
   const [activeOffer, setActiveOffer] = useState<Offer | undefined>(undefined);
 
+  let mainClassName = 'page__main page__main--index';
   const currentOffers = useAppSelector(getProcessedOffers);
+  if (currentOffers.length === 0) { mainClassName += ' page__main--index-empty'; }
 
   const currentCityName = useAppSelector(getCity);
   const offersNumber = currentOffers.length;
@@ -30,41 +32,57 @@ function MainScreen (): JSX.Element {
     return <LoadingScreen />;
   }
   // Пока не знаю как по другому исключить undefined из результата find..
-  if (!currentCity) {
+  if (!currentCity)
+  {
     return <NotFoundScreen />;
   }
 
   return (
-    <main className="page__main page__main--index">
+    <main className={mainClassName}>
       <h1 className="visually-hidden">Cities</h1>
       <div className="tabs">
         <section className="locations container">
           <TabListComponent />
         </section>
       </div>
-      <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{ `${offersNumber} places to stay in ${currentCityName}` }</b>
+      {
+        (currentOffers.length === 0) ?
+          <div className ="cities">
+            <div className="cities__places-container cities__places-container--empty container">
+              <section className="cities__no-places">
+                <div className="cities__status-wrapper tabs__content">
+                  <b className="cities__status">No places to stay available</b>
+                  <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+                </div>
+              </section>
+              <div className="cities__right-section">
+              </div>
+            </div>
+          </div> :
+          <div className="cities">
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{ `${offersNumber} places to stay in ${currentCityName}` }</b>
 
-            <SortingList />
+                <SortingList />
 
-            <OffersListScreen
-              className='cities__places-list tabs__content'
-              offers={currentOffers}
-              setActiveOffer={setActiveOffer}
-            />
-          </section>
-          <div className="cities__right-section">
-            <section className="cities__map map">
+                <OffersListScreen
+                  className='cities__places-list tabs__content'
+                  offers={currentOffers}
+                  setActiveOffer={setActiveOffer}
+                />
+              </section>
+              <div className="cities__right-section">
+                <section className="cities__map map">
 
-              <Map offers={currentOffers} city={{...currentCity.location, zoom: 13}} selectedPoint={activeOffer}></Map>
+                  <Map offers={currentOffers} city={{...currentCity.location, zoom: 13}} selectedPoint={activeOffer}></Map>
 
-            </section>
+                </section>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+      }
     </main>
   );
 }
